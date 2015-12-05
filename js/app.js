5fb4251c82fe3cbd0d7c22531d39029d31e491ec
Isaac Bueno <isaacmabueno@gmail.com>
2015-12-04 19:58:38 -0600

    nav broken but getting somewhere

--- js/app.js
+++ js/app.js
@@ -40,3 +40,17 @@ jQuery(function($) {
   });
 
 });
+
+$(document).ready(){
+  var  mn = $(".main-nav");
+    mns = "main-nav-scrolled";
+    hdr = $('header').height();
+
+$(window).scroll(function() {
+  if( $(this).scrollTop() > hdr ) {
+    mn.addClass(mns);
+  } else {
+    mn.removeClass(mns);
+  }
+});
+}
