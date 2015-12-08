fa80820bc7d36a594b635883e98575feb0497e9c
Isaac Bueno <isaacmabueno@gmail.com>
2015-12-08 13:44:07 -0600

    responsive nav

--- js/app.js
+++ js/app.js
@@ -40,17 +40,3 @@ jQuery(function($) {
   });
 
 });
-
-$(document).ready(){
-  var  mn = $(".main-nav");
-    mns = "main-nav-scrolled";
-    hdr = $('header').height();
-
-$(window).scroll(function() {
-  if( $(this).scrollTop() > hdr ) {
-    mn.addClass(mns);
-  } else {
-    mn.removeClass(mns);
-  }
-});
-}
